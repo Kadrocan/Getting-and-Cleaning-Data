6026e92cc0f21b0fc50e35c34b9270fd70f99973
Kadrocan <kadrocan@hotmail.com>
2015-11-23 21:46:10 +0200

    Week 3

---
+++ Week3/Q1.R
@@ -0,0 +1,18 @@
+# write the file url and file destination to an object
+file.url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv'
+file.dest <- 'ACS.csv'
+
+# download from the URL
+download.file(file.url, file.dest, method='curl' )
+
+# read the data
+ACS <- read.csv('ACS.csv')
+
+# create vector
+ACS$agricultureLogical <- ifelse(ACS$ACR==3 & ACS$AGS==6,TRUE,FALSE)
+
+# read lines
+which(ACS$agricultureLogical)
+
+    Status API Training Shop Blog About Pricing 
+
---
+++ Week3/Q2.R
@@ -0,0 +1,15 @@
+# write the file url and file destination to an object
+file.url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg'
+file.dest <- 'jeff.jpg'
+
+# download from the URL
+download.file(file.url, file.dest, mode='wb' )
+
+# load package
+library(jpeg)
+
+# load the data
+picture <- readJPEG('jeff.jpg', native=TRUE)
+
+# get the quantile info
+quantile(picture, probs = c(0.3, 0.8) )
